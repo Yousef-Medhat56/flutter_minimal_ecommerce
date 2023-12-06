efe559d6eb98a3835dc88566c11484ad09279df2
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-12-06 11:38:51 +0200

    feat: add the intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/widgets/my_button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // LOGO
+          Icon(
+            Icons.shopping_bag,
+            size: 72,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 24),
+          // TITLE
+          const Text(
+            "Minimal Shop",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 8),
+          // SUBTITLE
+          Text(
+            "Premium quality products",
+            style:
+                TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+          ),
+          const SizedBox(height: 24),
+          // BUTTON
+          MyButton(child: const Icon(Icons.arrow_forward), onTap: () {})
+        ],
+      )),
+    );
+  }
+}
