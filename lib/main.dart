223c0f2e28bad1399a47597f7549926f834d2854
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-12-09 08:21:42 +0200

    feat: install provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/models/shop.dart';
+import 'package:provider/provider.dart';
 import 'themes/light_mode.dart';
 import 'pages/intro_page.dart';
 import 'pages/shop_page.dart';
 import 'pages/cart_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => Shop(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
